6a9b7872b01f7cbb28dc804ede6a09624c859e6c
Samreaksa Ros <15318223+monkeyidesign@users.noreply.github.com>
2019-04-29 22:14:57 +0700

    Added top nav menu by using navigateController.

--- UITabBarController/UserProController.swift
+++ UITabBarController/UserProController.swift
@@ -12,6 +12,37 @@ class UserProController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView?.backgroundColor = .green
+        collectionView?.backgroundColor = .white
+
+        
+        let filter_img       = UIImage(named: "filter")!
+        let path_img   = UIImage(named: "path")!
+        
+        let filterButton      = UIBarButtonItem(image: filter_img,  style: .plain, target: self, action: #selector(didTapEditButton))
+        let refreshButton  = UIBarButtonItem(image: path_img,  style: .plain, target: self, action: #selector(didTapFavoriteButton))
+        
+        navigationItem.rightBarButtonItems = [filterButton, refreshButton]
+       
+        
+        let VTopLogo = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 150, height: 44.0))
+        let topLogo = UILabel(frame: CGRect(x: 0, y: 0.0, width: 150, height: 44.0))
+        topLogo.text = "MONKEY iDESIGN"
+        topLogo.lineBreakMode = NSLineBreakMode.byWordWrapping
+        VTopLogo.addSubview(topLogo)
+        
+        let leftButton = UIBarButtonItem(customView: VTopLogo)
+        self.navigationItem.leftBarButtonItem = leftButton
+        
+        
+        //navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named:"filter"), style: .plain, target: self, action: #selector(menuButtonTapped))
+    }
+    @objc func didTapEditButton(sender: UIBarButtonItem) {
+        
+        print("didTapEditButton")
+    }
+    @objc func didTapFavoriteButton(sender: UIBarButtonItem) {
+        
+        print("didTapFavoriteButton")
     }
 }
+
